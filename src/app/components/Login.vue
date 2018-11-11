47b50fc4b3c522696fc951d537188b9e8ab60780
unknown <drugoralex@gmail.com>
2018-11-11 18:28:36 -0500

    style

--- src/app/components/Login.vue
+++ src/app/components/Login.vue
@@ -2,8 +2,12 @@
     <div>
         <div class="container">
             <div class="row pt-5">
-                <img src="images/movie-night.png">
-                <div class="col-md-5">
+                <img src="images/movie-night.png" class="center">
+            </div>
+            <br>
+            <br>
+            <br>
+            <div class="col-md-5 center">
                     <div class="card">
                         <div class="card-body">
                             <!-- .prevent evita que la pagina se recargue y permita actualizar cierta parte de la vista de acuerdo a lo que se utilice en el metodo utilizado -->
@@ -14,12 +18,11 @@
                                 <div class="form-group">
                                     <input v-model="user.password" type="text" placeholder="Constraseña" class="form-control">
                                 </div>
-                                <button class="btn btn-primary btn-block">Send</button>
+                                <button class="btn btn-primary btn-block btn-style">Send</button>
                             </form>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
     </div> 
 </template>
